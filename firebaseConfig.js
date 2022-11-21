46f2b9d527ad4b8ef80d210fc92bc4d95ca3cd2a
Yash Agrawal <80036700+YashAgrawal213@users.noreply.github.com>
2022-11-21 21:58:53 +0530

    commit

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDFGeKMBv6YMeGUUPc-Et9mBFAeZPA5ltE",
+  authDomain: "agrawal-traders-37393.firebaseapp.com",
+  databaseURL: "https://agrawal-traders-37393-default-rtdb.firebaseio.com",
+  projectId: "agrawal-traders-37393",
+  storageBucket: "agrawal-traders-37393.appspot.com",
+  messagingSenderId: "308638040284",
+  appId: "1:308638040284:web:5369c51f447a5dae8d3419"
+};
+
+  firebase.initializeApp(firebaseConfig);
+  
---
+++ script.js
@@ -0,0 +1,74 @@
+var PhoneV, NameV, AddressV, EmailV;
+
+function readFom() {
+  PhoneV = document.getElementById("Phone").value;
+  NameV = document.getElementById("Name").value;
+  AddressV = document.getElementById("Address").value;
+  EmailV = document.getElementById("Email").value;
+  console.log(PhoneV, NameV, AddressV, EmailV);
+}
+
+document.getElementById("insert").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("AT/" + PhoneV)
+    .set({
+      Phone: PhoneV,
+      Name: NameV,
+      Address: AddressV,
+      Email: EmailV,
+    });
+  alert("Data Inserted");
+  document.getElementById("Phone").value = "";
+  document.getElementById("Name").value = "";
+  document.getElementById("Address").value = "";
+  document.getElementById("Email").value = "";
+};
+
+document.getElementById("read").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + PhoneV)
+    .on("value", function (snap) {
+      document.getElementById("Phone").value = snap.val().Phone;
+      document.getElementById("Name").value = snap.val().Name;
+      document.getElementById("Address").value = snap.val().Address;
+      document.getElementById("Email").value = snap.val().Email;
+    });
+};
+
+document.getElementById("update").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + PhoneV)
+    .update({
+      //   Phone: PhoneV,
+      Name: NameV,
+      Address: AddressV,
+      Email: EmailV,
+    });
+  alert("Data Update");
+  document.getElementById("Phone").value = "";
+  document.getElementById("Name").value = "";
+  document.getElementById("Address").value = "";
+  document.getElementById("Email").value = "";
+};
+document.getElementById("delete").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + PhoneV)
+    .remove();
+  alert("Data Deleted");
+  document.getElementById("Phone").value = "";
+  document.getElementById("Name").value = "";
+  document.getElementById("Address").value = "";
+  document.getElementById("Email").value = "";
+};
